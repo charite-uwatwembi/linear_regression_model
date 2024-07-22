9876271af7c36e15903be26df0a6a45e8b1b884c
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-07-22 20:29:48 +0200

    finalising flutter ui

--- summative/flutterApp/profits/lib/main.dart
+++ summative/flutterApp/profits/lib/main.dart
@@ -12,7 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Profit Prediction'),
+          title: Text('Profit Prediction'
+          , textAlign: TextAlign.center,),
+          backgroundColor: Colors.white, // AppBar color
         ),
         body: ProfitPredictionForm(),
       ),
@@ -32,76 +34,101 @@ class _ProfitPredictionFormState extends State<ProfitPredictionForm> {
   double marketingSpend = 0;
   String state = '';
 
-Future<void> _predictProfit() async {
-  try {
-    final response = await http.post(
-      Uri.parse('http://10.0.2.2:8000/predict'), // Ensure this is correct
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, dynamic>{
-        'rnd_spend': rndSpend,
-        'administration': administration,
-        'marketing_spend': marketingSpend,
-        'state': state,
-      }),
-    );
-
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
-
-    if (response.statusCode == 200) {
-      final result = jsonDecode(response.body);
-      print('Predicted Profit: ${result['profit']}');
-      // Show result in a dialog
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            content: Text('Predicted Profit: ${result['profit']}'),
-          );
+  Future<void> _predictProfit() async {
+    try {
+      final response = await http.post(
+        Uri.parse('http://10.0.2.2:8000/predict'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
         },
+        body: jsonEncode(<String, dynamic>{
+          'rnd_spend': rndSpend,
+          'administration': administration,
+          'marketing_spend': marketingSpend,
+          'state': state,
+        }),
       );
-    } else {
-      throw Exception('Failed to predict profit');
-    }
-  } catch (e) {
-    print('Error: $e');
-  }
-}
 
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
+
+      if (response.statusCode == 200) {
+        final result = jsonDecode(response.body);
+        print('Predicted Profit: ${result['profit']}');
+        // Show result in a dialog
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Text('Predicted Profit: ${result['profit']}'),
+            );
+          },
+        );
+      } else {
+        throw Exception('Failed to predict profit');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.purple[50]!, Colors.purple[200]!],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
       padding: const EdgeInsets.all(16.0),
       child: Form(
         key: _formKey,
         child: Column(
           children: <Widget>[
             TextFormField(
-              decoration: InputDecoration(labelText: 'R&D Spend'),
+              decoration: InputDecoration(
+                labelText: 'R&D Spend',
+                fillColor: Colors.white,
+                filled: true,
+              ),
               keyboardType: TextInputType.number,
               onChanged: (value) {
                 rndSpend = double.parse(value);
               },
             ),
+            SizedBox(height: 10),
             TextFormField(
-              decoration: InputDecoration(labelText: 'Administration'),
+              decoration: InputDecoration(
+                labelText: 'Administration',
+                fillColor: Colors.white,
+                filled: true,
+              ),
               keyboardType: TextInputType.number,
               onChanged: (value) {
                 administration = double.parse(value);
               },
             ),
+            SizedBox(height: 10),
             TextFormField(
-              decoration: InputDecoration(labelText: 'Marketing Spend'),
+              decoration: InputDecoration(
+                labelText: 'Marketing Spend',
+                fillColor: Colors.white,
+                filled: true,
+              ),
               keyboardType: TextInputType.number,
               onChanged: (value) {
                 marketingSpend = double.parse(value);
               },
             ),
+            SizedBox(height: 10),
             TextFormField(
-              decoration: InputDecoration(labelText: 'State'),
+              decoration: InputDecoration(
+                labelText: 'State',
+                fillColor: Colors.white,
+                filled: true,
+              ),
               onChanged: (value) {
                 state = value;
               },
@@ -109,9 +136,13 @@ Future<void> _predictProfit() async {
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: _predictProfit,
-              child: Text('Predict Profit'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.purple[200], // Button color
+              ),
+              child:  Text('Predict Profit', 
+                style: TextStyle(color: Colors.white),
             ),
-          ],
+        )],
         ),
       ),
     );
